e8effb91e108b486cdd79e072c73f02843129661
Xuelin Wang <xuelin_wang@apple.com>
2017-09-16 17:32:27 -0700

    break words

--- src/java/xl/learn/Uber.kt
+++
@@ -1,5 +0,0 @@
-package xl.learn
-
-/**
- * Created by xuelin on 9/16/17.
- */
--- src/java/xl/learn/WordBreak.java
+++ src/java/xl/learn/WordBreak.java
@@ -2,59 +2,104 @@ package xl.learn;
 
 import com.google.common.base.Strings;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
+ *
+ *
  * Created by xuelin on 5/3/17.
  */
 public class WordBreak {
-    public static List<List<String>> breakIt(String ss, List<String> words) {
-        Map<String, List<List<String>>> strToAnswers = new HashMap<>();
-        List<String> answer = new ArrayList<>();
-        answer.add("");
-        List<List<String>> answers = new ArrayList<>();
-        answers.add(answer);
-        strToAnswers.put("", answers);
-        return breakIt(ss, words, strToAnswers);
+
+    private static void addMatch(int from, String word, Map<Integer, Set<String>> prefixEndWords) {
+        int index = from + word.length();
+        prefixEndWords.putIfAbsent(index, new HashSet<>());
+        if (prefixEndWords.get(index).add(word));
     }
 
-    public static List<List<String>> breakIt(String ss, List<String> words, Map<String, List<List<String>>> strToAnswers) {
-        if (strToAnswers.containsKey(ss)) {
-            return strToAnswers.get(ss);
-        }
-
+    private static void breakItFrom(String ss, int from, Collection<String> words, Map<Integer, Set<String>> prefixEndWords) {
         for (String word: words) {
-            if (ss.startsWith(word)) {
-                List<List<String>> answers = breakIt(ss.substring(word.length()), words, strToAnswers);
-                List<List<String>> newAnswers =  strToAnswers.get(ss);
-                if (newAnswers == null) {
-                    newAnswers = new ArrayList<>();
-                    strToAnswers.put(ss, newAnswers);
-                }
-                for (List<String> answer: answers) {
-                    List<String> newAnswer = new ArrayList<>();
-                    newAnswer.add(word);
-                    newAnswer.addAll(answer);
-                    newAnswers.add(newAnswer);
-                }
+            String substr = ss.substring(from);
+            if (substr.startsWith(word)) {
+                addMatch(from, word, prefixEndWords);
             }
         }
-        return strToAnswers.get(ss);
     }
 
-    public static void main(String[] args0) {
-        String[] args = new String[]{
-                "catsanddog", "cat", "cats", "and", "sand", "dog"
-        };
-        String ss = args[0];
-        List<String> words = new ArrayList<>();
-        for (int i = 1; i < args.length; i++) {
-            words.add(args[i]);
+    public static List<List<String>> breakIt(String ss, Collection<String> dictWords) {
+        Map<Integer, Set<String>> prefixEndWords = new HashMap<>();
+        int from = 0;
+        while (from < ss.length()) {
+            breakItFrom(ss, from, dictWords, prefixEndWords);
+            if (from == 0 && prefixEndWords.isEmpty())
+                return Collections.emptyList();
+            final int finalFrom = from;
+            from = (prefixEndWords.keySet()).stream().filter(x -> x.intValue() > finalFrom).min(Comparator.<Integer>naturalOrder()).get();
         }
-        List<List<String>> answers = breakIt(ss, words);
+
+        List<List<String>> lists = new ArrayList<List<String>>();
+        List<String> list = new ArrayList<>();
+        list.add("");
+        lists.add(list);
+        return collect(ss.length(), prefixEndWords, lists);
+
+    }
+
+    private static List<List<String>> collect(int end, Map<Integer, Set<String>> prefixEndWords, List<List<String>> currLists) {
+        List<List<String>> newLists = new ArrayList<>();
+        for (String word: prefixEndWords.get(end)) {
+            List<List<String>> thisLists = new ArrayList<>();
+            for (List<String> list: currLists) {
+                ArrayList<String> newList = new ArrayList<>();
+                newList.addAll(list);
+                newList.add(0, word);
+                thisLists.add(newList);
+            }
+
+            if (end - word.length() > 0)
+                thisLists = collect(end - word.length(), prefixEndWords, thisLists);
+
+            newLists.addAll(thisLists);
+
+        }
+        return newLists;
+    }
+
+    public static List<List<String>> breakIt2(String ss, Collection<String> dictWords) {
+        int n = ss.length();
+        List<List<String>>[] dp = new List[n + 1];
+        List<List<String>> initial = new ArrayList<>();
+        List<String> newList = new ArrayList<>();
+        newList.add("");
+        initial.add(newList);
+        dp[0] = initial;
+
+        for (int i = 1; i < n+1; i++) {
+            List<List<String>> newLists = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                if (dictWords.contains(ss.substring(j, i))) {
+                    for (List<String> list: dp[j]) {
+                        newList = new ArrayList<>();
+                        newList.addAll(list);
+                        newList.add(ss.substring(j, i));
+                        newLists.add(newList);
+                    }
+                }
+            }
+            dp[i] = newLists;
+        }
+
+        return dp[n];
+    }
+
+
+    public static void main(String[] args0) {
+        String[] wordsArr = new String[]{
+                "cat", "cats", "and", "sand", "dog"
+        };
+        String ss = "catsanddog";
+        List<String> words = Arrays.asList(wordsArr);
+        List<List<String>> answers = breakIt2(ss, words);
         for (List<String> answer: answers) {
             System.out.println(String.join(" ", answer));
         }
