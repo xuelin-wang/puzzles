aff1c5f6031fef62fa91f22882afb8a6c7afa8a3
Xuelin Wang <xuelin_wang@apple.com>
2017-09-18 15:40:31 -0700

    allO1 data structure for inc, dec, getMax, getMin

--- src/java/xl/learn/AllO1.java
+++ src/java/xl/learn/AllO1.java
@@ -1,5 +1,7 @@
 package xl.learn;
 
+import java.util.*;
+
 /**
  *
  * Implement a data structure supporting the following operations:
@@ -16,28 +18,162 @@ package xl.learn;
  Challenge: Perform all these in O(1) time complexity.
 
  solution:
- constant by key, constant by value
+ ValueNode { int val; ValueNode next; ValueNode prev; ValueNode up; ValueNode down; }
+   use prev/next to link nodes with same value. use up/down to link next bigger/smaller node in the doubly linked list as explained below.
+ Map<Key, ValueNode> kvMap
 
- hash by key:
-     valBucket: value, count, keys, prevBucket, nextBucket
-   ConstOps {
-     Map<String, Integer> kvs;
-     Map<Integer, Bucket> countBuckets;
-     Bucket head, tail;
-   }
+ Double linked list of "head" ValueNodes to keep track of different values. A head ValueNode for a value is just any ValueNode with the value.
+ head and tail of this list remember min and max.
 
- Inc(Key) - kvs.get(key), put(key, 1) | not exists, otherwise put(key, inc(curval)). countBuckets[oldval]--, countBuckets[newVal]++.
-     remove from valBuckets when count to zero. adjust orderedCounts with new change.
- Dec(Key) - If Key's value is 1, remove it from the data structure.
- Otherwise decrements an existing key by 1. If the key does not exist, this function does nothing.
- Key is guaranteed to be a non-empty string.
- GetMaxKey() - Returns one of the keys with maximal value.
- If no element exists, return an empty string "".
- GetMinKey() - Returns one of the keys with minimal value.
- If no element exists, return an empty string "".
 
+ Map<Key, Cell> kvs: Cell is linked structure with left, right, up, down links.
+ Map<Integer, Cell> headers: map value to header for the value
+ mainHeader
+     |
+  header for i <--> key i1 <-->  key i2 <--> header
+     |
+  header for j <-->  key j1
+     |
+  header for k <-->  key k1 <--> key k2 <--> key k3 <-->
+     |
+ mainHeader
+
+ Inc(Key): from kvs, get its Cell. if exists, remove original cell from row. if list for the row is empty now, remove the header.
+     for new Cell with increased value, add it to the kvs map. if no row for new cell, insert new header row. add cell to the row.
+ Dec(Key): opposite operation from above.
+ GetMaxKey() - mainHeader.up.right.key
+ GetMinKey() - mainHeader.down.right.key
 
  */
 
-public class AllO1 {
+public class AllO1<T> {
+    public static class Cell<T> {
+        int val;
+        T key;
+        Cell<T> left;
+        Cell<T> right;
+        Cell<T> up;
+        Cell<T> down;
+
+        public Header(T key, int val, Cell left, Cell right, Cell up, Cell down) {
+            this.key = key;
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.up = up;
+            this.down = down;
+        }
+    }
+
+    final Map<T, Cell<T>> kvs;
+    final Map<Integer, Cell<T>> headers;
+    final Cell<T> mainHeader;
+
+    public AllO1() {
+        kvs = new HashMap<>();
+        headers = new HashMap<>();
+        mainHeader = new Cell(null, 0, null, null, null, null);
+        mainHeader.up = mainHeader;
+        mainHeader.down = mainHeader;
+        mainHeader.left = null;
+        mainHeader.right = null;
+    }
+
+    private void removeCell(Cell<T> cell) {
+        kvs.remove(cell.key);
+
+        int val = cell.val;
+        Cell<T> header = headers.get(val);
+
+        cell.left.right = cell.right;
+        cell.right.left = cell.left;
+
+        if (header.left == header) {
+            headers.remove(val);
+            header.up.down = header.down;
+            header.down.up = header.up;
+        }
+    }
+
+    private void addCell(Cell<T> cell, Cell<T> headerBelow) {
+        kvs.put(cell.key, cell);
+
+        int val = cell.val;
+        Cell<T> header = headers.get(val);
+
+        if (header == null) {
+            header = new Cell(null, val, null, null, null, null);
+            header.left = header;
+            header.right = header;
+            headers.put(val, header);
+
+            header.up = headerBelow.up;
+            header.down = headerBelow;
+
+            headerBelow.up.down = header;
+            headerBelow.up = header;
+        }
+
+        cell.left = header;
+        cell.right = header.right;
+        header.right.left = cell;
+        header.right = cell;
+    }
+
+    public void inc(T key) {
+        Cell<T> fromCell = kvs.get(key);
+        Cell<T> headerBelow;
+        final int fromVal, toVal;
+        if (fromCell != null) {
+            fromVal = fromCell.val;
+            headerBelow = headers.get(fromVal).down;
+            removeCell(fromCell);
+        }
+        else {
+            fromVal = 0;
+            headerBelow = mainHeader.down;
+        }
+        toVal = fromVal + 1;
+        headerBelow = headerBelow.val == toVal ? headerBelow.down : headerBelow;
+        Cell<T> toCell = new Cell(key, toVal, null, null, null, null);
+        addCell(toCell, headerBelow);
+    }
+
+
+    public void dec(T key) {
+        Cell<T> fromCell = kvs.get(key);
+        if (fromCell == null)
+            return;
+
+        Cell<T> headerBelow;
+        final int fromVal, toVal;
+        fromVal = fromCell.val;
+        headerBelow = headers.get(fromVal).up;
+        removeCell(fromCell);
+
+        toVal = fromVal - 1;
+        headerBelow = headerBelow.val == toVal ? headerBelow.down : headerBelow;
+        Cell<T> toCell = new Cell(key, toVal, null, null, null, null);
+        addCell(toCell, headerBelow);
+    }
+
+    public T getMaxKey() {
+        Cell<T> cell = mainHeader.up.right;
+        if (cell == null)
+            return null;
+        else
+            return cell.key;
+    }
+
+    public T getMinKey() {
+        Cell<T> cell = mainHeader.down.right;
+        if (cell == null)
+            return null;
+        else
+            return cell.key;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/java/xl/learn/uber/SumTargetPaths.java
@@ -0,0 +1,10 @@
+package xl.learn.uber;
+
+/**
+ * Created by xuelin on 9/17/17.
+ *
+ * List all paths that sum to a given target value.
+ * The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+ */
+public class SumTargetPaths {
+}
